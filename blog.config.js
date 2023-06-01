ef3eb65171babb12923b9b550168146b97865e0a
Yeyuxuan1 <130195420+Yeyuxuan1@users.noreply.github.com>
2023-06-02 03:45:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -12,8 +12,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '数字花园瀑布流', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小马tv', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '看小马拉大车上小马tv', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.xxx.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '数字花园', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
